796c9161eaf284f1fb8e1893582afc843e11f396
Alex Williamson <alex_williamson@rocketmail.com>
2019-10-18 18:27:31 +0100

    I added camera movement, and slightly changed the fear_script to allow the camera to follow the player when they respawn.

--- Nightlight/Assets/Scripts/fear_script.cs
+++ Nightlight/Assets/Scripts/fear_script.cs
@@ -7,12 +7,14 @@ public class fear_script : MonoBehaviour
     [Range(0, 1000)]
     public int fear = 1000;
     public bool isTouchingLight;
-
     public Vector3 respawn;
-
+    private GameObject Camera;
+    public Vector3 camera_respawn;
     private void Start()
     {
         respawn = new Vector3(0f, 0f);
+        Camera = GameObject.FindWithTag("MainCamera");
+        camera_respawn = new Vector3(0f, 0f, -10f);
     }
 
     private void Update()
@@ -29,6 +31,8 @@ public class fear_script : MonoBehaviour
         if (fear == 0 && !isTouchingLight)
         {
             gameObject.transform.position = respawn;
+            camera_respawn.x = respawn.x;
+            Camera.GetComponent<Transform>().position = camera_respawn;
         }
     }
 }
